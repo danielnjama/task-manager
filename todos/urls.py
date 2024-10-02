e3de6e3089a19e45d64486db89fbbe01c6a8ff02
danielnjama <danielnjama2015@gmail.com>
2024-10-02 18:50:23 +0300

    urls update

--- todos/urls.py
+++ todos/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 admin.site.site_header="DTECH TODOS"
 admin.site.site_title="DTECH TODOS"
@@ -10,4 +14,9 @@ admin.site.index_title="DTECH TODOS"
 
 urlpatterns = [
     path('', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    
 ]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns= urlpatterns+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
